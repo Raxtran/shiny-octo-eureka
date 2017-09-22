31723f282d45bf117e17f548a1eebbede65cc215
Alex <raxtran1@gmail.com>
2017-09-22 17:41:04 +0200

    Main2 subido

---
+++ Main2.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner at = new Scanner(System.in);
+        System.out.println("Frase: ");
+        String frase = at.nextLine();
+
+        int counter = 0;
+
+        for (int i = 0; i < frase.length(); i++) {
+
+            char pt = frase.charAt(i);
+
+            if(pt == 'a' || pt == 'e' || pt == 'i' || pt == 'o' || pt =='u'){
+                counter++;
+            }
+
+
+        }
+
+        System.out.println("Tiene "+ counter +" vocales");
+
+
+
+
+
+    }
+}
